778ef49b19e9d894ffa77b128b7f6eadec69c7c5
Nahuel José <nahueljose@gmail.com>
2018-11-04 22:06:07 -0300

    Basic webpack config with hot-reload + environments

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path                = require('path');
+const HtmlWebpackPlugin   = require('html-webpack-plugin');
+const CleanWebpackPlugin  = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const merge  = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
