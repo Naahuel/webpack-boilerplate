1ae229b688aafee22be98bb9f71673eeb481d5ea
Nahuel José <nahueljose@gmail.com>
2018-11-06 11:57:44 -0300

    Installed file loader and html loader

--- webpack.common.js
+++ webpack.common.js
@@ -22,13 +22,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-      rules: [{
-          test: /\.scss$/,
-          use: [
-              "style-loader", // creates style nodes from JS strings
-              "css-loader", // translates CSS into CommonJS
-              "sass-loader" // compiles Sass to CSS, using Node Sass by default
-          ]
-      }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+            // creates style nodes from JS strings
+            "style-loader",
+            // translates CSS into CommonJS
+            "css-loader",
+            // compiles Sass to CSS, using Node Sass by default
+            "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            // Handles image files inserted in HTML and CSS
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name][hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            // Handles the HTML output, particularly useful for inserted assets to be processed by other loaders
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
   }
 };
